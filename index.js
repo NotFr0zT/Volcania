fe9c62d3795344db8b1cd715aaa181291338b03c
Fr0zT-code <69401786+Fr0zT-code@users.noreply.github.com>
2021-05-28 18:01:27 +0200

    Removed guildCreate event

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ client.aliases = new Collection();
 });
 
 
+client.on('guildCreate', async guild => {
+	let defaultChannel = 'general';
+    guild.channels.cache.forEach((channel) => {
+        if (channel.type == 'text' && defaultChannel == 'general') {
+            if (channel.permissionsFor(guild.me).has('SEND_MESSAGES')) {
+                defaultChannel = channel;
+            }
+        }
+    })
+
+    const newserverjoinembed = new MessageEmbed()
+        .setTitle('Hello!')
+        .setDescription('Hello there! Thanks for inviting me to your server! Need help? Type `v!help`')
+        .addField('Important Links:', `[Invite Link](https://discord.com/api/oauth2/authorize?client_id=843428824385716244&permissions=8&scope=bot)\n[Support Server](https://discord.gg/xyqpAvyPgZ)`)
+        .setColor('BLUE')
+        .setTimestamp();
+    defaultChannel.send(newserverjoinembed)
+})
+
 
 keepAlive();
 client.login(process.env.TOKEN);
