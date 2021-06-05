17e130e6555189a1526e95395814459e13d06e15
Fr0zT-code <69401786+Fr0zT-code@users.noreply.github.com>
2021-06-05 09:13:16 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -16,7 +16,7 @@ module.exports = async (client) => {
 
 	setInterval(function () {
 		const status = botStatus[Math.floor(Math.random() * botStatus.length)];
-		client.user.setPresence({ activity: { type: 'WATCHING', url: "https://www.twitch.tv/", name: status }, status: 'dnd' });
+		client.user.setPresence({ activity: { type: 'STREAMING', url: "https://www.twitch.tv/fr0zttt", name: status } });
 	}, 30000);
 
 	console.log(`Logged in as ${client.user.tag}`);
