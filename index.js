b5d82add010fc8af215b0ad721c605acf8835726
Fr0zT-code <69401786+Fr0zT-code@users.noreply.github.com>
2021-06-05 09:09:28 +0200

    Fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const keepAlive = require('./server');
 const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
 require('discord-reply');
 const db = require('quick.db');
-const client = new Client({ disableMentions: 'everyone', partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER', 'GUILD_MEMBER'], ws: { intents: Intents.ALL, properties: { $browser: "Discord iOS" } } });
+const client = new Client({ disableMentions: 'everyone', partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER', 'GUILD_MEMBER'], ws: { intents: Intents.ALL } });
 require('discord-buttons')(client);
 
 client.commands = new Collection();
@@ -15,25 +15,25 @@ client.aliases = new Collection();
 
 
 client.on('guildCreate', async (guild) => {
-    // let defaultChannel = 'general';
-    // guild.channels.cache.forEach((channel) => {
-    //     if (channel.type == 'text' && defaultChannel == 'general') {
-    //         if (channel.permissionsFor(guild.me).has('SEND_MESSAGES')) {
-    //             defaultChannel = channel;
-    //         }
-    //     }
-    // })
+    let defaultChannel = 'general';
+    guild.channels.cache.forEach((channel) => {
+        if (channel.type == 'text' && defaultChannel == 'general') {
+            if (channel.permissionsFor(guild.me).has('SEND_MESSAGES')) {
+                defaultChannel = channel;
+            }
+        }
+    })
 
-    // db.set(`moderation_${guild.id}`, true)
+    db.set(`moderation_${guild.id}`, true)
 
-    // defaultChannel.send(new MessageEmbed()
-    //     .setTitle('Thank you for adding me!')
-    //     .setDescription('Hello there! Thanks for inviting me to your server! Need help? Type `v!help`')
-    //     .addField('Important Links:', `[Invite Link](https://discord.com/api/oauth2/authorize?client_id=843428824385716244&permissions=8&scope=bot)\n[Support Server](https://discord.gg/xyqpAvyPgZ)\n [Github Repository](https://github.com/NotFr0zT/Volcania)`)
-    //     .setColor('BLUE')
-    //     .setFooter(`Note: Moderation is enabled by default | Turn it off by v!moderation off`)
-    //     .setTimestamp()
-    // )
+    defaultChannel.send(new MessageEmbed()
+        .setTitle('Thank you for adding me!')
+        .setDescription('Hello there! Thanks for inviting me to your server! Need help? Type `v!help`')
+        .addField('Important Links:', `[Invite Link](https://discord.com/api/oauth2/authorize?client_id=843428824385716244&permissions=8&scope=bot)\n[Support Server](https://discord.gg/xyqpAvyPgZ)\n [Github Repository](https://github.com/NotFr0zT/Volcania)`)
+        .setColor('BLUE')
+        .setFooter(`Note: Moderation is enabled by default | Turn it off by v!moderation off`)
+        .setTimestamp()
+    )
     // const welcomebutton = new MessageButton()
     //     .setStyle('green')
     //     .setLabel('Hey there, im Volcania! Do you need help? Simply click me!')
